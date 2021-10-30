40333fc8d0427ca1036deefebebe7f57709794d9
colder <cncolder@gmail.com>
2021-10-31 01:01:54 +0800

    chore: git ignore compiled files

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Check if a value is empty. Includes undefined, null, empty string, NaN, empty array, empty
- * object, empty Set, empty Map, or array/set/map/object with empty values.
- */
-export declare function isEmptyValue(value: any): boolean;
